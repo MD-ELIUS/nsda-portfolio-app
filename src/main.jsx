8d861e53ea4aae3145a3f6f8653150e8de074535
MD-ELIUS <mdelius.nfs@gmail.com>
2026-01-01 21:14:17 +0600

    Project Setup Completed

---
+++ src/main.jsx
@@ -0,0 +1,33 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import RootLayout from './layouts/RootLayout.jsx';
+import Home from './pages/Home.jsx';
+import About from './pages/About.jsx';
+import Portfolio from './pages/Portfolio.jsx';
+import Service from './pages/Service.jsx';
+import Contact from './pages/Contact.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    Component: RootLayout,
+    children: [
+      { index: true, Component: Home },
+      { path: "about", Component: About },
+      { path: "portfolio", Component: Portfolio },
+      { path: "service", Component: Service },
+      { path: "contact", Component: Contact },
+     
+    ],
+  },
+]);
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+   <RouterProvider router={router} />
+  </StrictMode>,
+)
